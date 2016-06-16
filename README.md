2fa5cca02a447b136e1d8a03938c6deff3005400
Christopher J. Brody <brodybits@litehelpers.net>
2016-06-16 09:32:45 +0200

    Initial version with simple test alert

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cordova SQL test app
+
+AUTHOR: @brodybits
+
+LICENSE: MIT or Apache 2.0
---
+++ www/test.js
@@ -0,0 +1,3 @@
+document.addEventListener('deviceready', function() {
+  alert('received deviceready event');
+});
